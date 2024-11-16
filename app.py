d3672bbe22a0a03ca11974f6544ba5eccbee37c6
sumitkumarbhagat <sumitbhagat2301@gmail.com>
2024-11-16 20:30:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ def performance():
     return render_template('performance.html', r2_score=r2_score_value, mae=mae, overall_accuracy=overall_accuracy, plot_url=plt_image)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable (default to 5000)
+    app.run(host='0.0.0.0', port=port)
